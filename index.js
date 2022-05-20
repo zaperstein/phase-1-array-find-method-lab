c78b00dc1dde0244494084ee00ea13d03b4964ea
Maggie Saperstein <maggie.saperstein@gmail.com>
2022-05-20 11:11:03 -0500

    commit tree 7b8aa7e298b13620f6e5e542911f0b643214373d

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // code your solution here
+
+
+function superbowlWin(win) {
+  
+ let date = win.find(pizza => pizza.result === 'W') 
+    if (date){
+      return date.year
+    } else {
+      return undefined
+    }
+  // date ? date.year : undefined;
+  }
+  
